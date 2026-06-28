809c0baaa4ed07b648f0012e4827c0401ecc6e24
Sevi C <me@seviche.cc>
2026-06-28 21:29:58 +0800

    Update Next.js

--- src/app/q/page.tsx
+++ src/app/q/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 type SearchRecord = {
